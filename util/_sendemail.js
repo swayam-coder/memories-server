acc8b3ee9dc5fc576444a4d50424b8085e319e80
swayam-coder <swayam14feb@gmail.com>
2021-08-14 21:32:31 +0530

    security update

--- util/_sendemail.js
+++ util/_sendemail.js
@@ -2,11 +2,6 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 require('dotenv').config()
 
-// const REFRESH_TOKEN = "1//0417a3B3_ZCqSCgYIARAAGAQSNwF-L9IraZUXv3IIFlT08Nzxhg8xsuNdFh964K0w-ZKmN7Ci0vdyEfgsDQhSrNmQSzSxAQODoDM"
-// const CLIENT_ID = "316313260853-ank2llsjqro4nf3163si8uacircoiplh.apps.googleusercontent.com"
-// const CLIENT_SECRET = "UZBrSl5lFkkwxc-jQ58moHsX"
-// const REDIRECT_URL = "https://developers.google.com/oauthplayground"
-
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
